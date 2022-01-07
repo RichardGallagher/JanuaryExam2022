1911e13d13ec19e4012183d2aadee82f24c206e7
RichardGallagher <78158578+RichardGallagher@users.noreply.github.com>
2022-01-07 17:37:21 +0000

    Added renewal date and sample data

--- JanuaryExam2022/MainWindow.xaml.cs
+++ JanuaryExam2022/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace JanuaryExam2022
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Member> members = new List<Member>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+        JuniorMember jm1 = new JuniorMember() { Name = "Jack Murphy", Fee = 1000, JoinDate = new DateTime(2020, 5, 7), PaymentType = PaymentSchedule.Annual };
+        JuniorMember jm2 = new JuniorMember() { Name = "Emily Kelly", Fee = 1000, JoinDate = new DateTime(2021, 1, 10), PaymentType = PaymentSchedule.Biannual };
+        JuniorMember jm3 = new JuniorMember() { Name = "Daniel Ryan", Fee = 1000, JoinDate = new DateTime(2020, 11, 30), PaymentType = PaymentSchedule.Monthly };
+
+        Member m1 = new Member() { Name = "Ella Doyle", Fee = 1000, JoinDate = new DateTime(2019, 3, 20), PaymentType = PaymentSchedule.Annual };
+        Member m2 = new Member() { Name = "Fionn Kennedy", Fee = 1000, JoinDate = new DateTime(2018, 8, 15), PaymentType = PaymentSchedule.Biannual };
+        Member m3 = new Member() { Name = "Louise Moore", Fee = 1000, JoinDate = new DateTime(2017, 2, 10), PaymentType = PaymentSchedule.Monthly };
+
+        SeniorMember sm1 = new SeniorMember() { Name = "Cian Daly", Fee = 1000, JoinDate = new DateTime(2015, 4, 24), PaymentType = PaymentSchedule.Annual };
+        SeniorMember sm2 = new SeniorMember() { Name = "Bobby Quinn", Fee = 1000, JoinDate = new DateTime(2014, 12, 1), PaymentType = PaymentSchedule.Biannual };
+        SeniorMember sm3 = new SeniorMember() { Name = "Eve Gallagher", Fee = 1000, JoinDate = new DateTime(2013, 6, 1), PaymentType = PaymentSchedule.Monthly };
+
+
+        
     }
 }
---
+++ JanuaryExam2022/Member.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam2022
+{
+    public enum PaymentSchedule
+    {
+        Annual,
+        Biannual,
+        Monthly
+    }
+    public class Member
+    {
+        //properties
+        public string Name { get; set; }
+
+        public DateTime JoinDate { get; set; }
+
+        public decimal Fee { get; set; }
+
+        //Constructors
+
+        //Methods
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+        private DateTime renewalDate;
+       
+        public DateTime RenewalDate 
+        {
+            get
+            {
+                renewalDate = JoinDate.AddYears(1);
+                return renewalDate;
+            } 
+        }
+    }
+}
